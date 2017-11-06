be2e4a28460af7b483c0c28978b8472ee6cf7d4f
Meher <mehersam251@gmail.com>
2017-11-06 08:47:55 -0700

    Uploaded a set of input fcs files, created main Activity. Need to still connect Matlab.

--- src/SBOL_TASBE_Connector/Connector.java
+++ src/SBOL_TASBE_Connector/Connector.java
@@ -1,10 +1,13 @@
 package SBOL_TASBE_Connector;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 
 import org.sbolstandard.core2.Activity;
+import org.sbolstandard.core2.Agent;
 import org.sbolstandard.core2.Collection;
+import org.sbolstandard.core2.Plan;
 import org.sbolstandard.core2.SBOLDocument;
 import org.sbolstandard.core2.SBOLValidationException;
 import org.synbiohub.frontend.SynBioHubException;
@@ -13,28 +16,58 @@ import org.synbiohub.frontend.SynBioHubFrontend;
 public class Connector {
 
 	private SynBioHubFrontend hub;
-	private SBOLDocument doc; 
+	private SBOLDocument built_doc; 
+	private SBOLDocument col_doc; 
 	private Collection fcs_col; 
 	private File script;
 	
-	public Connector(String url, String prefix, String email, String pass, File read_file, String id, String version, String name,
-			String desc, boolean complete, boolean create_defaults, URI _fcs_col, File _script_plan) throws SynBioHubException
+	public Connector(String prefix, String email, String pass, boolean complete, boolean create_defaults) throws SynBioHubException
 	{
 		hub = new SynBioHubFrontend(prefix, prefix);
-		doc = hub.getSBOL(_fcs_col); //should be a doc with a collection of uris
+		hub.login(email, pass);
+		
+		built_doc = new SBOLDocument();
+		built_doc.setDefaultURIprefix(prefix);
+		built_doc.setComplete(complete);
+		built_doc.setCreateDefaults(create_defaults);
 		
 	}
 	
-	public void create_Activities(String displayId) throws SBOLValidationException
+	public void submit(String id, String version, String name, String desc, SBOLDocument doc) throws SynBioHubException
 	{
-		Activity a = doc.createActivity(displayId);
-		
-		//assign a plan
-		
-		//assign an agent
+		hub.submit(id, version, name, desc, "", "", "1", doc);
 
 	}
 	
+	//retrieve the collection of fcs files
+	public void get_Collection(URI _fcs_col, String col_dispId, String col_version) throws SynBioHubException, SBOLValidationException
+	{
+		col_doc = hub.getSBOL(_fcs_col); //should be a doc with a collection of uris	
+		Collection c = col_doc.getCollection(col_dispId, col_version); //list of fcs files. 
+		
+	}
+	
+	public void create_Activity(String activity_name, String plan_prefix, String agent_prefix,  String version, String _plan, String _usage, String _agent ) throws SBOLValidationException, IOException
+	{
+		Activity a = built_doc.createActivity(activity_name);
+		a.createUsage(_usage, fcs_col.getIdentity());
+	
+		//assign a plan and an agent
+		built_doc.createPlan(plan_prefix, _plan, version); //uri to a script - take this run with Matlab
+		retrieve_execute_plan(plan_prefix); 
+		built_doc.createAgent(agent_prefix, _agent, version); 
+	}
+	
+	public Collection retrieve_execute_plan(String fileURL) throws IOException
+	{
+		//make a get request to get a file
+		
+		HttpDownloadUtility.downloadFile(fileURL, "");
+
+		//call matlab with the file
+		
+		return null; 
+	}
 	
 	
 	
--- src/SBOL_TASBE_Connector/Connector_Tester.java
+++ src/SBOL_TASBE_Connector/Connector_Tester.java
@@ -1,10 +1,72 @@
 package SBOL_TASBE_Connector;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+
+import org.sbolstandard.core2.Collection;
+import org.sbolstandard.core2.GenericTopLevel;
+import org.sbolstandard.core2.SBOLDocument;
+import org.sbolstandard.core2.SBOLValidationException;
+import org.synbiohub.frontend.SynBioHubException;
+import org.synbiohub.frontend.SynBioHubFrontend;
+
 public class Connector_Tester {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws URISyntaxException, SBOLValidationException, SynBioHubException {
+		
+		String prefix = "https://synbiohub.utah.edu";
+		String email = "mehersam251@gmail.com";
+		String pass = "S@ipav12";
+		
+		String id = "TASBE_Tutorial_Example_Controls";
+		String version = "1";
+		String name = "Example_Controls";
+		String description = "Example controls containing .fcs files"; 
+		
+		String topLevel = "https://synbiohub.utah.edu/user/mehersam/" + id + id + "_" + "/" + version; 
+		URI TP_collection = URI.create(topLevel); 
+		
+		SBOLDocument document = new SBOLDocument();
+		document.setDefaultURIprefix(prefix);
+		document.setComplete(true);
+		document.setCreateDefaults(true);
+		
+		//build an SBOLDocument with fcs input files
+		Set<GenericTopLevel> fcs_files = new HashSet<GenericTopLevel>(); 
+		
+		fcs_files.add(document.createGenericTopLevel(
+				"fcs_1",
+				"",
+				new QName("https://github.com/jakebeal/TASBEFlowAnalytics-Tutorial/raw/develop/example_controls/", "07-29-11_A_006_P3.fcs", "pr")));
+	
+		fcs_files.add(document.createGenericTopLevel(
+				"fcs_2",
+				"",
+				new QName("https://github.com/jakebeal/TASBEFlowAnalytics-Tutorial/raw/develop/example_controls/", "07-29-11_B_006_P3.fcs", "pr")));
+		
+		fcs_files.add(document.createGenericTopLevel(
+				"fcs_3",
+				"",
+				new QName("https://github.com/jakebeal/TASBEFlowAnalytics-Tutorial/raw/develop/example_controls/", "07-29-11_C_006_P3.fcs", "pr")));
+		
+		Collection c = document.createCollection("Input_fcs"); 
+		for(GenericTopLevel gtl : fcs_files)
+			c.addMember(gtl.getIdentity()); 
 
+		SynBioHubFrontend fb = new SynBioHubFrontend(prefix, prefix);
+		fb.login(email, pass);
+		fb.submit(id, version, name, description, "", "", "1", document);
+		
+		//add it as a source?
+		
+		
+			
+		
+		
 	}
 
 }
